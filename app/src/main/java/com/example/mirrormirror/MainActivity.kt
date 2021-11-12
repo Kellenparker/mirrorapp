be46f8079ea020b36b833bd6d16277c4aed04ce0
Cody Fenn <cjfenn1@buffs.wtamu.edu>
2021-11-11 21:05:33 -0600

    Loading Screen

--- app/src/main/java/com/example/mirrormirror/MainActivity.kt
+++ app/src/main/java/com/example/mirrormirror/MainActivity.kt
@@ -5,15 +5,19 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.ListView
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import java.util.*
 
 
@@ -36,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         val timeBtn = findViewById<Button>(R.id.timeBtn)
         val capBtn = findViewById<Button>(R.id.capBtn)
         val listLinks = findViewById<ListView>(R.id.linksListView)
+        val loadingIcon = findViewById<ImageView>(R.id.loading)
         val timeRef = database.getReference("modules/time/disabled")
         var tBool = true
         timeRef.setValue(true);
@@ -51,41 +56,58 @@ class MainActivity : AppCompatActivity() {
         }
 
         val capRef = database.getReference("scan/camera/capture")
+        var dataReference = database.getReference()
         lateinit var mdatabase: DatabaseReference
         mdatabase = Firebase.database.reference
 
 
-//        DO NOT TOUCH!!!!!!!!!!!!!!!! lol
         capBtn.setOnClickListener {
             capRef.setValue(true)
-            for (i in 0..45){
-                mdatabase.child("scan/links/link$i/linkUrl").get().addOnSuccessListener{
-                    reference = it.value.toString()
-                    listItems.add(reference)
-                    mdatabase.child("scan/links/link$i/linkTitle").get().addOnSuccessListener{
-                        reference2 = it.value.toString()
-                        listItems2.add(reference2)
-                        mdatabase.child("scan/links/link$i/linkImg").get().addOnSuccessListener{
-                            reference3 = it.value.toString()
-                            listItems3.add(reference3)
-                            mdatabase.child("scan/stage").get().addOnSuccessListener() {
-                                val myListAdapter = MyListAdapter(this, listItems, listItems2, listItems3)
-                                listLinks.adapter = myListAdapter
+            dataReference.child("scan/stage").addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val stageRef = dataSnapshot.getValue().toString()
+                    if (stageRef == "3"){
+                        loadingIcon.isVisible = false
+                        listLinks.isVisible = true
+                        for (i in 0..45){
+                            mdatabase.child("scan/links/link$i/linkUrl").get().addOnSuccessListener{
+                                reference = it.value.toString()
+                                listItems.add(reference)
+                                mdatabase.child("scan/links/link$i/linkTitle").get().addOnSuccessListener{
+                                    reference2 = it.value.toString()
+                                    listItems2.add(reference2)
+                                    mdatabase.child("scan/links/link$i/linkImg").get().addOnSuccessListener{
+                                        reference3 = it.value.toString()
+                                        listItems3.add(reference3)
+                                        mdatabase.child("scan/stage").get().addOnSuccessListener() {
+                                            val myListAdapter = MyListAdapter(this@MainActivity, listItems, listItems2, listItems3)
+                                            listLinks.adapter = myListAdapter
+                                        }
+                                    }
+                                }
                             }
                         }
+                        listLinks.setOnItemClickListener { parent, _, position, _ ->
+                            val selectedItem = parent.getItemAtPosition(position) as String
+                            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                            openURL.data = Uri.parse("https://www.amazon.com" + selectedItem)
+                            startActivity(openURL)
+                        }
+                    }else{
+                        loadingIcon.isVisible = true
+                        listLinks.isVisible = false
+                        Glide.with(this@MainActivity).load(R.drawable.loadingicon2).into(loadingIcon)
                     }
                 }
-            }
 
-            listLinks.setOnItemClickListener { parent, _, position, _ ->
-                val selectedItem = parent.getItemAtPosition(position) as String
-                val openURL = Intent(android.content.Intent.ACTION_VIEW)
-                openURL.data = Uri.parse("https://www.amazon.com" + selectedItem)
-                startActivity(openURL)
-            }
-
+                override fun onCancelled(error: DatabaseError) {
+                    //This doesn't do anything but will cause errors if you delete it
+                }
+            })
         }
+
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == SECOND_ACTIVITY_REQUEST_CODE) {
